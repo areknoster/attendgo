2564651796a23463a01048ba39219936dba6820a
Arkadiusz Noster <arkadiusz.noster@gmail.com>
2022-05-26 14:04:47 +0200

    feat: working solution on embedded linux

--- cmd/attendgo/main.go
+++ cmd/attendgo/main.go
@@ -15,16 +15,25 @@ import (
 	"github.com/areknoster/attendgo/memstorage"
 	"github.com/areknoster/attendgo/photo"
 	"github.com/areknoster/attendgo/photo/linuxcapturer"
+	photosaver "github.com/areknoster/attendgo/photo/saver"
 	"github.com/areknoster/attendgo/signals"
 	"github.com/areknoster/attendgo/signals/gpio"
-	"github.com/areknoster/attendgo/signals/memio"
 	"github.com/areknoster/attendgo/test/testsubscribers"
+	"github.com/areknoster/attendgo/webui"
+	"github.com/sethvargo/go-envconfig"
 	"github.com/warthog618/gpiod/device/rpi"
+	"golang.org/x/sync/errgroup"
 )
+
+type Config struct {
+	webui.ServerConfig
+}
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
+	cfg := Config{}
+	handleFatal(envconfig.Process(ctx, &cfg))
 
 	ps := mempubsub.NewPubSub(mempubsub.Config{BufSize: 20})
 	defer ps.Close()
@@ -32,10 +41,12 @@ func main() {
 	ps.Register(testsubscribers.Printer{})
 	ps.Register(id.NewHandler(ps))
 
-	kp, closer := GPIO(ps)
-	defer closer.Close()
+	kb, closeKeyboard := InitKeyboard(ps)
+	defer closeKeyboard.Close()
 
-	attendeeStorage := memstorage.NewAtendeeStorage()
+	photoStorage := memstorage.NewPhotoStorage()
+	ps.Register(photosaver.New(photoStorage))
+
 	decoder, err := linuxcapturer.NewYUYVDecoder()
 	if err != nil {
 		log.Fatal("get YUYV decoder: ", err)
@@ -48,14 +59,44 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	theNotary := notary.New(ps, attendeeStorage, photographer)
-	ps.Register(theNotary)
 
-	ps.Register(testsubscribers.PhotoSaver{Dir: "."})
-	log.Fatal(kp.Run(ctx))
+	attendeeStorage := memstorage.NewAtendeeStorage()
+	nt := notary.New(ps, attendeeStorage, photographer)
+	ps.Register(nt)
+
+	ps.Register(InitSignalHandler())
+
+	uiHandler, err := webui.NewUIHandler(photoStorage)
+	handleFatal(err)
+	srv := webui.NewServer(cfg.ServerConfig, uiHandler, webui.NewAttendeesHandler(attendeeStorage))
+
+	eg, ctx := errgroup.WithContext(ctx)
+	withCtx := func(r domain.Runner) func() error {
+		return func() error {
+			return r.Run(ctx)
+		}
+	}
+	eg.Go(withCtx(kb))
+	eg.Go(withCtx(srv))
+
+	handleFatal(eg.Wait())
 }
 
-func GPIO(ps domain.Publisher) (*keypad.Keypad, closer) {
+func handleFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func InitSignalHandler() *signals.SignalHandler {
+	return signals.NewSignalHandler(signals.SignalOutputs{
+		Good:    gpio.NewGPIOOut(rpi.GPIO22),
+		Warning: signals.NamedPrinterDecorator{Name: "warning"},
+		Bad:     gpio.NewGPIOOut(rpi.GPIO27),
+	})
+}
+
+func InitKeyboard(ps domain.Publisher) (*keypad.Keypad, closer) {
 	// 1 and 5 work
 	cl := closer{}
 	greenDiode := gpio.NewGPIOIn(rpi.GPIO23)
@@ -66,16 +107,21 @@ func GPIO(ps domain.Publisher) (*keypad.Keypad, closer) {
 		SweepInteval:      10 * time.Millisecond,
 		WaitBetweenInputs: 300 * time.Millisecond,
 		Rows: [4]signals.Input{
+			gpio.NewGPIOIn(rpi.GPIO21),
 			gpio.NewGPIOIn(rpi.GPIO9),
 			gpio.NewGPIOIn(rpi.GPIO11),
-			memio.NewMemIO(),
-			memio.NewMemIO(),
+			gpio.NewGPIOIn(rpi.GPIO20),
+			// memio.NewMemIO(),
 		},
 		Columns: [4]signals.Output{
-			gpio.NewGPIOOut(rpi.GPIO10),
+			// signals.NamedPrinterDecorator{},
+			// signals.NamedPrinterDecorator{},
+			// signals.NamedPrinterDecorator{},
+			// signals.NamedPrinterDecorator{},
+			gpio.NewGPIOOut(rpi.GPIO26),
 			gpio.NewGPIOOut(rpi.GPIO12),
-			memio.NewMemIO(),
-			memio.NewMemIO(),
+			gpio.NewGPIOOut(rpi.GPIO25),
+			gpio.NewGPIOOut(rpi.GPIO8),
 		},
 	}
 	return keypad.New(ps, cnf), cl
